65f7de5afe24dbf1313f3dbdf8221358c41acc50
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-04-26 19:54:47 -0300

    Criados atributos de instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,18 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=47):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano')
     print(Pessoa.cumprimentar(p))
-
     print(id(p))
-
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Adriano'
+    print(p.nome)
+    print(p.idade)
