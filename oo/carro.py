21d274643b002740e280339d977adb6c749b6e53
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-04-27 21:00:56 -0300

    Criados os métodos calcular_velocidade, acelerar, frear, calcular_direcao, girar_a_direita, girar_a_esquerda na classe Carro.

--- oo/carro.py
+++ oo/carro.py
@@ -71,24 +71,24 @@ O   L
     >>> direcao.valor
     'Norte'
     >>> carro = Carro(direcao, motor)
-    >>> carro.calcular_velocidade
+    >>> carro.calcular_velocidade()
     0
     >>> carro.acelerar()
     >>> carro.calcular_velocidade()
     1
     >>> carro.acelerar()
-    >>> carro.calcular_velocidade
+    >>> carro.calcular_velocidade()
     2
     >>> carro.frear()
-    >>> carro.calcular_velocidade
+    >>> carro.calcular_velocidade()
     0
-    >>> carro.calcular_direcao)
+    >>> carro.calcular_direcao()
     'Norte'
     >>> carro.girar_a_direita()
-    >>> carro.calcular_direcao
+    >>> carro.calcular_direcao()
     'Leste'
     >>> carro.girar_a_esquerda()
-    >>> carro.calcular_direcao
+    >>> carro.calcular_direcao()
     'Norte'
     >>> carro.girar_a_esquerda()
     >>> carro.calcular_direcao()
@@ -100,6 +100,24 @@ class Carro:
     def __init__(self, direcao, motor):
         self.direcao = direcao
         self.motor = motor
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
 
 
 NORTE = 'Norte'
